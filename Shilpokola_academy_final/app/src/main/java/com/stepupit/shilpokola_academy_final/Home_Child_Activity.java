624494343d0878a4814625ebd04c3e5b9a52664a
Md. Abdul Malek <44054338+sajibmalek@users.noreply.github.com>
2020-09-06 12:52:07 +0600

    custom style
    custom style

---
+++ Shilpokola_academy_final/app/src/main/java/com/stepupit/shilpokola_academy_final/Home_Child_Activity.java
@@ -0,0 +1,14 @@
+package com.stepupit.shilpokola_academy_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Home_Child_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home__child_);
+    }
+}
---
+++ Shilpokola_academy_final/app/src/main/java/com/stepupit/shilpokola_academy_final/Kontho_shilpi_Activity.java
@@ -0,0 +1,14 @@
+package com.stepupit.shilpokola_academy_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Kontho_shilpi_Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kontho_shilpi_);
+    }
+}
